fc0aed48eab7dd788d513140ea90ee0ba254782c
BautistaTordini <bautistatordini.uns@gmail.com>
2024-11-22 13:15:22 -0300

    chore: Refactor TwitterFollowCard component to use formatUserName prop for displaying the username

--- project/HolaMundo/src/App.jsx
+++ project/HolaMundo/src/App.jsx
@@ -2,10 +2,13 @@ import './App.css';
 import { TwitterFollowCard } from './TwitterFollowCard';
 
 export function App(){
+    const formatUserName = (userName) => `@${userName}`
     return (
         <section className='App'>
-            <TwitterFollowCard userName="bautordini" name="Juan Bautista Tordini"/>
-            <TwitterFollowCard userName="BautistaTordini" name="Juan Bautista Tordini"/>
+            <TwitterFollowCard formatUserName={formatUserName} isFollowing userName="JuanBautistaTordini" name="Juan Bautista Tordini"/>
+            <TwitterFollowCard formatUserName={formatUserName} isFollowing userName="" name="lorem ipsum"/>
+            <TwitterFollowCard formatUserName={formatUserName} isFollowing userName="" name="lorem ipsum"/>
+            <TwitterFollowCard formatUserName={formatUserName} isFollowing userName="" name="lorem ipsum"/>
         </section>
     )
 }
--- project/HolaMundo/src/TwitterFollowCard.jsx
+++ project/HolaMundo/src/TwitterFollowCard.jsx
@@ -1,16 +1,17 @@
 import './App.css';
 
-export function TwitterFollowCard({userName, name, isFollowing}) {
+export function TwitterFollowCard({formatUserName, userName, name, isFollowing}) {
+    console.log(isFollowing)
     return (
         <article className='tw-followCard'>
             <header className='tw-followCard-header'>
                 <img 
                     className='tw-followCard-avatar'
-                    src={`https://unavatar.io/${userName}`}
+                    src={`https://unavatar.io/github/${userName}`}
                     alt="PorfileTwitterCard" />
                 <div className='tw-followCard-info'>
                     <strong>{name}</strong>
-                    <span className='tw-followCard-infoUserName'>@{userName}</span>
+                    <span className='tw-followCard-infoUserName'>{formatUserName(userName)}</span>
                 </div>
             </header>
 
