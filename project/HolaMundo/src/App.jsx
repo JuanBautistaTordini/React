81e2e5c0ab4df6f6e1041ef30a692af040569616
BautistaTordini <bautistatordini.uns@gmail.com>
2024-11-22 13:00:13 -0300

    refactor: Update CSS class names and structure for follow card component

--- project/HolaMundo/src/App.jsx
+++ project/HolaMundo/src/App.jsx
@@ -3,9 +3,9 @@ import { TwitterFollowCard } from './TwitterFollowCard';
 
 export function App(){
     return (
-        <>
+        <section className='App'>
             <TwitterFollowCard userName="bautordini" name="Juan Bautista Tordini"/>
             <TwitterFollowCard userName="BautistaTordini" name="Juan Bautista Tordini"/>
-        </>
+        </section>
     )
 }
