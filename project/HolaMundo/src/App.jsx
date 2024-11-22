a3e735e435e3a438f6d027ab35f6e5189829e79b
BautistaTordini <bautistatordini.uns@gmail.com>
2024-11-22 12:26:57 -0300

    FirstCommit - React Projects

---
+++ project/HolaMundo/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css';
+
+export function App(){
+    return (
+        <article>
+            <header>
+                <img src="https://portfolio-tordinibautista.netlify.app/assets/img/img-portfolio.png" alt="PorfileTwitterCard" />
+                <div>
+                    <strong>Juan Bautista Tordini</strong>
+                    <span>@BautistaTordini</span>
+                </div>
+            </header>
+
+            <aside>
+                <button>
+                    Seguir
+                </button>
+            </aside>
+        </article>
+    )
+}
